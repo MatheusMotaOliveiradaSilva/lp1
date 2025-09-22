8d163d23631329ab261b74845f5baef38d9c3082
Matheus Mota Oliveira da Silva <theguim7@gmail.com>
2025-09-21 21:12:45 -0500

    Add files via upload

--- lista6(vetores)/ex1.c
+++ lista6(vetores)/ex1.c
@@ -1,35 +1,33 @@
 #include <stdio.h>
 
 void preenche(int* xs, int n) {
-	for (int i = 0; i < n; i++) {
-		printf("inclua os elementos do vetor\n");
-	    scanf("%d", &xs[i]);
-	}
+    for (int i = 0; i < n; i++) {
+        printf("inclua os elementos do vetor\n");
+        scanf("%d", xs + i); 
+    }
 }
 
-	void mostra(int* xs, int n) {
-		for (int i = 0; i < n; i++) {
-            printf("\n%d\n", xs[i]);
+void mostra(int* xs, int n) {
+    for (int i = 0; i < n; i++) {
+        printf("\n%d\n", *(xs + i)); 
     } 
-	}
+}
 
-    float media(int* xs, int n) {
-        int soma = 0;
-        for (int j = 0; j < n; j++) {
-            soma += xs[j];
-        }
-        return (float)soma / n;
-    }        
+float media(int* xs, int n) {
+    int soma = 0;
+    for (int j = 0; j < n; j++) {
+        soma += *(xs + j); 
+    }
+    return (float)soma / n;
+}        
 
-void main (void) {
-	int x;
-	printf("Digite o tamanho do vetor\n");
-	scanf("%d", &x);
-	int vec [x];
-	preenche(vec, x);
+void main(void) {
+    int x;
+    printf("Digite o tamanho do vetor\n");
+    scanf("%d", &x);
+    int vec[x];
+    preenche(vec, x);
     mostra(vec, x);
     float m = media(vec, x);
-    printf("\nmédia: %f", m);
-
-
+    printf("\nmédia: %f\n", m);
 }
